0193edf65d8d2ffa29c20d4439318a08617132e7
Derek Hickey <derekhickey7@gmail.com>
2026-02-07 19:35:35 -0800

    add evaluation metrics to math_utils.py

--- linear-regression-from-scratch/src/math_utils.py
+++ linear-regression-from-scratch/src/math_utils.py
@@ -7,8 +7,31 @@ def mse(y_true, y_pred):
 
     return np.mean((y_true - y_pred) ** 2)
 
+# Calculate the mean absolute error loss
 def mae(y_true, y_pred):
-    
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    return np.mean(abs(y_true - y_pred))
+
+# Calculate the root mean squared error loss
+def rmse(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    return mse(y_true, y_pred) ** 0.5
+
+# Calculate the R-squared
+def r2(y_true, y_pred):
+    y_true = np.asarray(y_true)
+    y_pred = np.asarray(y_pred)
+
+    var_y = np.var(y_true)
+
+    if var_y == 0:
+        return 0
+    else:
+        return 1 - (mse(y_true, y_pred)/np.var(y_true))
 
 # Compute gradients for weight and bias on given dataset
 def compute_gradients(X, y, w, b) -> tuple:
