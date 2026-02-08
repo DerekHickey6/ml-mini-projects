681ca599b42e85db3de0cecc6ed1fd51d287ebb4
Derek Hickey <derekhickey7@gmail.com>
2026-02-07 19:59:09 -0800

    add final weight and bias to metrics, minor format change

--- linear-regression-from-scratch/src/evaluate.py
+++ linear-regression-from-scratch/src/evaluate.py
@@ -8,9 +8,9 @@ from model import LinearRegressionGD
 X_data, y_data = load_clean_LR_w_noise()
 
 # Split to training
-X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X_data, y_data, test_size=0.2, random_state=42)
 
-# Fit data
+# Fit data to training data
 reg = LinearRegressionGD(lr=3e-5, epochs=300)
 reg.fit(X_train, y_train)
 
@@ -29,12 +29,17 @@ train_mae = mae(y_true=y_train, y_pred=train_pred)
 test_mae = mae(y_true=y_test, y_pred=test_pred)
 
 train_r2 = r2(y_true=y_train, y_pred=train_pred)
-test_r2 = r2(y_true=y_train, y_pred=train_pred)
+test_r2 = r2(y_true=y_test, y_pred=test_pred)
 
 # Display Evaluation metrics
-eval = pd.DataFrame([[train_mse, test_mse], [train_rmse, test_rmse], [train_mae, test_mae], [train_r2, test_r2]],
-                    index=['MSE', 'RMSE','MAE','R^2'],
-                    columns=["Train", "Test"])
-eval["Difference"] = eval["Train"] - eval["Test"]
+eval_df = pd.DataFrame([[test_mse, train_mse], [test_rmse, train_rmse], [test_mae, train_mae], [test_r2, train_r2]],
+                    index=['MSE', 'RMSE','MAE','R²'],
+                    columns=["Test", "Train"])
+
+eval_df["Difference"] = eval_df["Test"] - eval_df["Train"]
+
 print("     --- Evaluation Metrics --- ")
-print(eval)
+print(eval_df)
+print(f"Final Weight: {reg.w_:0.4f}")
+print(f"Final Bias:   {reg.b_:0.4f}")
+
