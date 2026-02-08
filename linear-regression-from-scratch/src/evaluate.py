7fa037498316200ee131c7be09669cde83cd5560
Derek Hickey <derekhickey7@gmail.com>
2026-02-07 19:46:10 -0800

    update evalute.py to show additional metrics in dataframe

--- linear-regression-from-scratch/src/evaluate.py
+++ linear-regression-from-scratch/src/evaluate.py
@@ -1,6 +1,7 @@
 from sklearn.model_selection import train_test_split
 from data.datasets import load_clean_LR_w_noise
-from math_utils import mse
+from math_utils import mse, rmse, mae, r2
+import pandas as pd
 from model import LinearRegressionGD
 
 # Load data
@@ -21,4 +22,18 @@ test_pred = reg.predict(X_test)
 train_mse = mse(y_true=y_train, y_pred=train_pred)
 test_mse = mse(y_true=y_test, y_pred=test_pred)
 
+train_rmse = rmse(y_true=y_train, y_pred=train_pred)
+test_rmse = rmse(y_true=y_test, y_pred=test_pred)
 
+train_mae = mae(y_true=y_train, y_pred=train_pred)
+test_mae = mae(y_true=y_test, y_pred=test_pred)
+
+train_r2 = r2(y_true=y_train, y_pred=train_pred)
+test_r2 = r2(y_true=y_train, y_pred=train_pred)
+
+# Display Evaluation metrics
+eval = pd.DataFrame([[train_mse, test_mse], [train_rmse, test_rmse], [train_mae, test_mae], [train_r2, test_r2]],
+                    index=['MSE', 'RMSE','MAE','R^2'],
+                    columns=["Train", "Test"])
+eval["Difference"] = eval["Train"] - eval["Test"]
+print(eval)
