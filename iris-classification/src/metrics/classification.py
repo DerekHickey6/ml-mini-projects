674caf428459c07329e2cf48c3dd51e44d491801
Derek Hickey <derekhickey7@gmail.com>
2026-02-08 20:11:43 -0800

    added macro_precision() and macro_recall() for confusion matrices

--- iris-classification/src/metrics/classification.py
+++ iris-classification/src/metrics/classification.py
@@ -1,11 +1,11 @@
 import numpy as np
 
 def confusion_matrix(y_true, y_pred, n_classes):
-    # Checking Shape error
+    # Test for Shape error
     if y_true.shape != y_pred.shape:
         raise ValueError(f"Shape mismatch: {y_true.shape} and {y_pred.shape} do not match")
 
-    # Checking bounds
+    # Test for bounds
     if y_true.min() < 0 or y_true.max() >= n_classes:
         raise ValueError("y_true contains labels outside valid range [0, n_classes)")
 
@@ -22,14 +22,55 @@ def confusion_matrix(y_true, y_pred, n_classes):
     return conf_mat
 
 def accuracy_score(y_true, y_pred):
-    # Checking Shape error
+    # Test for Shape error
     if y_true.shape != y_pred.shape:
         raise ValueError(f"Shape mismatch: {y_true.shape} and {y_pred.shape} do not match")
+
+
+    # Test for empty input
+    if not (y_true and y_pred):
+        raise ValueError("Input must be non-empty")
 
     # Count the matching pairs and divide by total
     accuracy = np.sum([x == y for x, y in zip(y_true, y_pred)]) / len(y_true)
 
     return accuracy
 
+def macro_precision(conf_mat):
 
+    accum_precisions = 0
+    length = len(conf_mat)
+
+    # Sum of all precisions
+    for i in range(length):
+        tp = conf_mat[i, i]
+        fp = np.sum(conf_mat[:, i]) - tp
+
+        if (tp + fp) != 0:
+            accum_precisions += tp / (tp + fp)
+        else:
+            accum_precisions += 0
+
+    return accum_precisions / length
+
+
+def macro_recall(conf_mat):
+
+    accum_recall = 0
+    length = len(conf_mat)
+
+    # Sum of all recalls
+    for i in range(length):
+        tp = conf_mat[i, i]
+        fn = np.sum(conf_mat[i, :]) - tp
+
+        if (tp + fn) != 0:
+            accum_recall += tp / (tp + fn)
+        else:
+            accum_recall += 0
+
+    return accum_recall / length
+
+def f1(conf_mat):
+    pass
 
