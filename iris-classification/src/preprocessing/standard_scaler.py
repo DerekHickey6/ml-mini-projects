ede36e2dfbc006556cfd2718a2288fa195806f1b
Derek Hickey <derekhickey7@gmail.com>
2026-02-10 22:31:17 -0800

    add edge case testing to StandardScaler.py

--- iris-classification/src/preprocessing/standard_scaler.py
+++ iris-classification/src/preprocessing/standard_scaler.py
@@ -3,7 +3,7 @@ import numpy as np
 class StandardScaler:
     def __init__(self):
         self.mean_ = None
-        self.std_ = np.sqrt(self.var_) if self.var_ != 0 else 1
+        self.std_ = None
         self.var_ = None
         self.n_features_ = None
 
@@ -17,29 +17,40 @@ class StandardScaler:
         if X.shape[0] < 1 or X.shape[1] < 1:
             raise ValueError("Shape mismatch: Array must have at least 1 row and 1 column")
 
-        self.mean_ = np.mean(X)
-        self.var_ = np.var(X)
+        self.mean_ = np.mean(X, axis=0)
+        self.var_ = np.var(X,axis=0)
         self.n_features_ = X.shape[1]
+        self.std_ = np.sqrt(self.var_)
+
+        # Handles zero-variance cases to avoid div by 0
+        self.std_[self.std_ == 0] = 1
+
 
         return self
 
     # returns X_scaled
     def transform(self, X):
         # Enforces fit call before transform
-        if self.mean_ is None or self.var_ is None or self.n_features_ is None:
+        if self.std_ is None:
             raise ValueError("Fit() must be called before transform()")
 
+        # ensures X is 2D array
+        if X.ndim != 2:
+            raise ValueError("Shape mismatch: X must be shape (N, D)")
+
         # Ensures data to be transformed has same num features
-        if X.shape[1] is not self.n_features_:
+        if X.shape[1] != self.n_features_:
             raise ValueError("Number of features does not match number learned in fit()")
+
+        # Enforces array shape
+        if X.shape[0] < 1 or X.shape[1] < 1:
+            raise ValueError("Shape mismatch: Array must have at least 1 row and 1 column")
 
         return (X - self.mean_) / self.std_
 
     # Convenience method for first time fitting + transforming
     def fit_transform(self, X):
-        self.mean_ = np.mean(X)
-        self.var_ = np.var(X)
 
+        self.fit(X)
 
-
-        return (X - self.mean_) / self.std_
+        return self.transform(X)
