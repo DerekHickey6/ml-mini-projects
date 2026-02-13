07cd87078b627bd2c9e60c308187078c03d13e97
Derek Hickey <derekhickey7@gmail.com>
2026-02-12 16:03:14 -0800

    fix bugs in train_Test_Split and add additional test cases

--- iris-classification/src/preprocessing/train_test_split.py
+++ iris-classification/src/preprocessing/train_test_split.py
@@ -1,13 +1,23 @@
 import numpy as np
 
 def train_test_split(X, y, test_size=0.2, shuffle=False, random_seed=1):
-    # Enforces array shape
-    if X.shape[0] < 1 or X.shape[1] < 1:
-        raise ValueError("Shape mismatch: Array must have at least 1 row and 1 column")
+    X = np.array(X)
+    y = np.array(y)
+
+    # Ensure test_size is valid
+    if test_size > 1 or test_size < 0:
+        raise ValueError("Test_size must be between 0 and 1")
 
     # ensures X is 2D array
     if X.ndim != 2:
         raise ValueError("Shape mismatch: X must be shape (N, D)")
+
+    # Enforces array shape
+    if X.shape[0] < 1 or X.shape[1] < 1:
+        raise ValueError("Shape mismatch: Array must have at least 1 row and 1 column")
+
+    if y.ndim != 1:
+        raise ValueError("y should be 1-dimension")
 
     if len(X) != len(y):
         raise ValueError("X data and y data must be the same length")
@@ -16,14 +26,15 @@ def train_test_split(X, y, test_size=0.2, shuffle=False, random_seed=1):
     length = len(X)
     split_index = int(length * (1 - test_size))
 
-    X = np.array(X)
-    y = np.array(y)
-
     # Shuffle Data
-    if shuffle == True:
+    if shuffle:
         np.random.seed(random_seed)
-        np.random.shuffle(X)
-        np.random.shuffle(y)
+
+        idxs = list(range(length))
+        np.random.shuffle(idxs)
+
+        X = X[idxs]
+        y = y[idxs]
 
     # Split the data
     X_train = X[:split_index]
