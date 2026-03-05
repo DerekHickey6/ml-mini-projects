a18af4996b9a9823930708625fff2e9656c60796
Derek Hickey <derekhickey7@gmail.com>
2026-03-05 12:13:40 -0800

    add encode_binary_columns(df) to preprocessing.py

---
+++ customer-survival-analysis/src/preprocessing.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas as pd
+
+# map columns [gender, parent, dependents, phone service, paperlessBilling] to 1/0
+def encode_binary_columns(df):
+    for col in df.columns:
+        if set(df[col].unique()) <= {'Yes', 'No'}:
+            df[col] = df[col].map({'Yes' : 1, 'No': 0})
+
+        if set(df[col].unique()) <= {'Male', 'Female'}:
+            df[col] = df[col].map({'Male': 1, 'Female': 0})
+
+    return df
+
+# One-hot encode multi-category columns
+# multipleLines, InternetService, onlinesecurity, onlineBackup, deviceprotection, techsupport, streamingTV, StreamingMovies, Contract, PaymentMethod
+def encode_categorical_columns(df):
